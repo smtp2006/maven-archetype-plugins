f0aebc136a416a12fe48226da56037dbb4dcd5a8
王华(061021) <hua.wanghuawh@alibaba-inc.com>
2014-07-16 14:57:48 +0800

    update

---
+++ springmvc-quickstart/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $github.smtp2006.maven.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
